36af65b166c8acc6d64bf625ac9fc9d455bf3770
Léo Fazendeiro <fazendeiro.leo@gmail.com>
2016-03-24 15:53:57 +0100

    Test commit

--- PokerConsole/cartes.h
+++ PokerConsole/cartes.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+Type énuméré comprenant les différentes valeurs que peut prendre la couleur d'une carte
+**/
 typedef enum Couleur Couleur;
 
 enum Couleur
@@ -16,7 +18,8 @@ Structure permettant de stocker les informations d'une carte.
 Une carte est caractérisée par sa couleur (coeur, carreau, pique ou trèfle),
 et par sa valeur ( de 1 à 13 )
 **/
-struct Carte {
- int iValeur; // Valeur de la carte
- Couleur cCouleur; // Couleur de la carte
-}Carte;
+struct Carte
+{
+    int iValeur; // Valeur de la carte
+    Couleur cCouleur; // Couleur de la carte
+} Carte;
