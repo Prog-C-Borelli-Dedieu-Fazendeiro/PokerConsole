42ee49d8fae7aeb11566b0016b81f30295c31ac0
borellig <geoffrey.borelli@gmail.com>
2016-03-24 16:19:50 +0100

    Ajout Joueur c & h
    joueur.c joueur.h

---
+++ PokerConsole/Joueur.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Joueur.h>
+
+
+/*** author : Geoffrey BORELLI ***/
---
+++ PokerConsole/Joueur.h
@@ -0,0 +1,9 @@
+#include <cartes.h>
+
+typedef struct Joueur
+{
+    char cPseudo[10];
+	int iCapital;
+	int iEtat;
+
+}Joueur;
