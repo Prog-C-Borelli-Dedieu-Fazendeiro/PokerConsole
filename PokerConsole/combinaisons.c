06c5c7f027c2a845e936bcfb6ed4fa38adc25d7e
dedieu <yoandedieu@gmail.com>
2016-04-04 23:26:55 +0200

    Commit Combinaisons
    Ajout de paire simple/carte_haute dans tous les cas possibles. La
    plupart des autres fonctions vont en découler.

--- PokerConsole/combinaisons.c
+++ PokerConsole/combinaisons.c
@@ -1,3 +1,239 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "combinaisons.h"
+
+
+int valeur_combinaison(int iCartes_joueur[2], int iCartes_communes[5])
+{
+    int iValeur_combinaison = 0;
+
+    int iTableau_cartes[7];
+
+    // Remplissage du tableau avec les valeurs de chaque cartes.
+    for(int i = 0; i < 2; i++)
+    {
+        iTableau_cartes[i] = iCartes_joueur[i];
+    }
+
+    for(int i = 0; i < 5; i++)
+    {
+        iTableau_cartes[i+2] = iCartes_communes[i];
+    }
+
+    for(int i = 0; i < 7; i++)
+    {
+        printf("\nCarte %i : %i",i+1 , iTableau_cartes[i]);
+    }
+
+    // On test ici toutes les combinaisons en commençant par les plus hautes.
+    // Si l’on trouve une combinaison la valeur de celle-ci est enregistrée dans la variable iValeur_combinaison
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = quinte_royale(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = quinte_flush(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = carre(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = full(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = couleur(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = suite(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = brelan(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = double_paire(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = simple_paire(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        iValeur_combinaison = carte_haute(iTableau_cartes, 7);
+    }
+    if(iValeur_combinaison == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return iValeur_combinaison;
+    }
+}
+
+int quinte_royale(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int quinte_flush(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int carre(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int full(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int couleur(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int suite(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int brelan(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int double_paire(int iTableau_cartes[], int iNombre_cartes)
+{
+    return 0;
+}
+
+int simple_paire(int iTableau_cartes[], int iNombre_cartes)
+{
+    if (iNombre_cartes > 0)
+    {
+        int iValeur_combinaison = 0;
+        int iValeur_combinaison_temp = 0;
+        int iNombre_cartes_restantes = iNombre_cartes;
+        int * iTableau_cartes_restantes;
+        iTableau_cartes_restantes = malloc( iNombre_cartes_restantes * sizeof(int));
+        if(iTableau_cartes_restantes == NULL)
+        {
+            printf("Allocation impossible");
+            exit(EXIT_FAILURE);
+        }
+        for(int i = 0; i < iNombre_cartes_restantes; i++)
+        {
+            iTableau_cartes_restantes[i] = iTableau_cartes[i];
+        }
+        while(iNombre_cartes_restantes != 0)
+        {
+            for(int i = 1; i < iNombre_cartes_restantes; i++)
+            {
+                // On test s’il s’agit d’une paire avec la première carte du tableau
+                // S’il s’agit d’une paire, on rentre la valeur de la carte dans iValeur_combinaison_temp
+                // sinon on lui donne la valeur 0
+                iValeur_combinaison_temp = iTableau_cartes_restantes[0] == iTableau_cartes_restantes[i] ? iTableau_cartes_restantes[i] : 0;
+                // On modifie iValeur_combinaison uniquement si on a une meilleure paire.
+                if((iValeur_combinaison_temp != 0) & (iValeur_combinaison < iValeur_combinaison_temp))
+                {
+                    iValeur_combinaison = iValeur_combinaison_temp;
+                }
+            }
+            iNombre_cartes_restantes -= 1;
+            if (iNombre_cartes_restantes > 0)
+            {
+                free(iTableau_cartes_restantes);
+                iTableau_cartes_restantes = malloc( iNombre_cartes_restantes * sizeof(int));
+                if(iTableau_cartes_restantes == NULL)
+                {
+                    printf("Allocation impossible");
+                    exit(EXIT_FAILURE);
+                }
+                for(int i = 0; i < iNombre_cartes_restantes; i++)
+                {
+                    iTableau_cartes_restantes[i] = iTableau_cartes[iNombre_cartes - iNombre_cartes_restantes + i];
+                }
+            }
+        }
+        int iCartes_joueur[2];
+        int iCartes_communes[5];
+
+        for(int i = 0; i < 2; i++)
+        {
+            if(iTableau_cartes[i] == iValeur_combinaison)
+            {
+                iCartes_joueur[i] = 0;
+            }
+            else
+            {
+                iCartes_joueur[i] = iTableau_cartes[i];
+            }
+        }
+
+        for(int i = 0; i < 5; i++)
+        {
+            if(iTableau_cartes[i+2] == iValeur_combinaison)
+            {
+                iCartes_communes[i] = 0;
+            }
+            else
+            {
+                iCartes_communes[i] = iTableau_cartes[i+2];
+            }
+        }
+
+        // L’ordre de grandeur d’une paire est de 10000 ajouté à la valeur d’une combinaison et de la carte haute.
+        // On définit la carte haute soit à partir de la main du joueur si le joueur dispose d’une paire à partir d’une
+        // ou aucune carte de sa main soit à partir des cartes communes uniquement si le joueur à sa paire dans
+        // sa propre main.
+        // Exemple de valeur : Paire de dames avec 10 comme carte haute -> 11210.
+        if((iTableau_cartes[0] != iTableau_cartes[1]) & (iValeur_combinaison !=0))
+        {
+            iValeur_combinaison = 10000 + iValeur_combinaison*100 + carte_haute(iCartes_joueur, 2)/100;
+        }
+        else if(iValeur_combinaison !=0)
+        {
+            iValeur_combinaison = 10000 + iValeur_combinaison*100 + carte_haute(iCartes_communes, 5)/100;
+        }
+        return iValeur_combinaison;
+    }
+    else
+    {
+        printf("Nombre de cartes invalide");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int carte_haute(int iTableau_cartes[], int iNombre_cartes)
+{
+    if (iNombre_cartes > 0)
+    {
+        int iValeur_combinaison = 0;
+        int iValeur_combinaison_temp = 0;
+        for(int i = 0; i < iNombre_cartes; i++)
+        {
+            iValeur_combinaison_temp = iTableau_cartes[0] <= iTableau_cartes[i] ? iTableau_cartes[i] : 0;
+            // On modifie iValeur_combinaison uniquement si on a une meilleure carte.
+            if((iValeur_combinaison_temp != 0) & (iValeur_combinaison < iValeur_combinaison_temp))
+            {
+                iValeur_combinaison = iValeur_combinaison_temp;
+            }
+        }
+        return (iValeur_combinaison*100)+iValeur_combinaison;
+    }
+    else
+    {
+        printf("Nombre de cartes invalide");
+        exit(EXIT_FAILURE);
+    }
+}
--- PokerConsole/combinaisons.h
+++ PokerConsole/combinaisons.h
@@ -1,7 +1,158 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/// Auteur :Yoan Dedieu
+/**
+ * \file combinaisons.h
+ * \brief DECLARARIONS DES PROTOTYPES LIES AUX COMBINAISONS
+ * \author Dedieu Y.
+ * \version 0.1
+ * \date 04/04/2016
+ *
+ * Ensemble de fonctions permettant de connaître la combinaison d'une main donnée pour un joueur.
+ * Score des combinaisons : un score est donné pour une combinaison (grâce à un entier) à chacune des combinaisons
+ * afin de pouvoir les comparer au mieux. Ce score est définie sur un entier à 5 chiffre :
+ *    + Chaque combinaison est noté de 00000 (Pour la carte simple) à 90000
+ *    + Chaque carte qui donne la hauteur de la combinaison est notée de 0100 (pour l’as dans le cas ou il est pris
+ *      en valeur faible) à 1400 (pour l’as dans le cas ou il est pris en valeur haute) de 100 en 100
+ *    + En plus des combinaison, en cas d’égalité, c’est la carte la plus haute dans la main du joueur qui est prise en compte,
+ *      cette carte est notée de 1 à 14 (As en valeur faible à As en valeur haute) de 1 en 1.
+ *      Si celle-ci ne peut définir à nouveau un gagnant (cas d’une paire directement en main) on regardera la carte
+ *      la plus haute dans les cartes communes.
+ * On additionne alors ces note ce qui donne un score entre 00101 (pour Combinaison : carte simple |
+ * hauteur de la combinaison : As | Carte la plus haute : As) à 91414 points
+ * (pour Combinaison : Quinte Royale | hauteur de la combinaison : As | Carte la plus haute : As).
+ */
 
 
+/**
+ * \fn valeur_combinaison(int iCartes_joueur[2], int iCartes_communes[5])
+ * \brief  Fonction permettant de calculer la valeur d'une main donnée;
+ *         utilise les fonctions permettant de décomposer chaque combinaison.
+ *
+ * \param iCartes_joueur[2] Tableau d'entier : Tableau contenant les valeurs des 2 cartes d'un joueur
+ *        iCartes_communes[5] Tableau d'entier : Tableau contenant les valeurs des 5 cartes communes
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int valeur_combinaison(int iCartes_joueur[2], int iCartes_communes[5]);
 
+/**
+ * \fn quinte_royale(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'une quinte flush royale
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int quinte_royale(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn quinte_flush(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'une quinte flush
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int quinte_flush(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn carre(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'un carré
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int carre(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn full(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'un full
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int full(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn couleur(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'une couleur
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int couleur(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn suite(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'une suite
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int suite(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn brelan(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'un brelan
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int brelan(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn double_paire(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'une double paire
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int double_paire(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn simple_paire(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir s'il s'agit d'une simple paire
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int simple_paire(int iTableau_cartes[], int iNombre_cartes);
+
+/**
+ * \fn carte_haute(int iTableau_cartes[], int iNombre_cartes)
+ * \brief  Fonction permettant de savoir la carte haute d'en ensmble de carte.
+ *
+ * \param iTableau_cartes[] Tableau d'entier : Tableau contenant les valeurs des cartes à tester pour la combinaison
+ *        iNombre_cartes Entier : Représente la taille du tableau
+ *
+ * \return un entier positif correspondant à la valeur d'une combinaison d'un joueur.
+ *
+ */
+int carte_haute(int iTableau_cartes[], int iNombre_cartes);
